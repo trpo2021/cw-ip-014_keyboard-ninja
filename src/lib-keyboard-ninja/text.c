a09511e19491ba8019ed7d54ead81eb7c469014e
bessonov <box@email.ru>
2021-06-01 01:34:28 +0700

    Created void read_text function

---
+++ src/lib-keyboard-ninja/text.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void read_text (char text[512], char identifier[], char filename[]){
+	FILE *base;
+	int i;
+	char string[512+16];
+	char path[256] = "../texts/";
+	strcat(path,filename);
+	base = fopen(path,"r");
+	
+	while(fgets(string,528,base)){
+		if(!strncmp(identifier,string,strlen(identifier))){
+			for(i = strlen(identifier); i < strlen(string); i++){
+				text[i - strlen(identifier)] = string[i];
+			}
+		}
+	}
+	
+	fclose (base);
+}
