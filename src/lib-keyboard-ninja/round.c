33016e5c357694ae987675c8affb407b0c8f639e
surrepoff <alexander.bessonov548@gmail.com>
2021-06-02 01:56:06 +0700

    Added empty start_round function

--- src/lib-keyboard-ninja/round.c
+++ src/lib-keyboard-ninja/round.c
@@ -1,1 +1,8 @@
+#include <lib-keyboard-ninja/auxiliary.h>
 #include <lib-keyboard-ninja/round.h>
+
+void start_round(
+        char identifier[MAX_LENGTH_OF_IDENTIFIER],
+        char text[MAX_LENGTH_OF_TEXT])
+{
+}
--- src/lib-keyboard-ninja/round.h
+++ src/lib-keyboard-ninja/round.h
@@ -1,4 +1,10 @@
 #ifndef ROUND_H
 #define ROUND_H
 
+#include <lib-keyboard-ninja/auxiliary.h>
+
+void start_round(
+        char identifier[MAX_LENGTH_OF_IDENTIFIER],
+        char text[MAX_LENGTH_OF_TEXT]);
+
 #endif /* ROUND_H */
