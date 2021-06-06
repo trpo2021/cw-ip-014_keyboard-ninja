9c8c1c4749d5d26e02c3f7f19fb20f80d0ace9b5
surrepoff <alexander.bessonov548@gmail.com>
2021-06-06 17:34:31 +0700

    Formated code

--- src/lib-keyboard-ninja/round.c
+++ src/lib-keyboard-ninja/round.c
@@ -5,46 +5,49 @@
 #include <lib-keyboard-ninja/auxiliary.h>
 #include <lib-keyboard-ninja/round.h>
 
-void print_round_interface(char identifier[MAX_LENGTH_OF_IDENTIFIER], char text[MAX_LENGTH_OF_TEXT], c_w current_word)
+void print_round_interface(
+        char identifier[MAX_LENGTH_OF_IDENTIFIER],
+        char text[MAX_LENGTH_OF_TEXT],
+        c_w current_word)
 {
     printf("\033[1mROUND | IDENTIFIER \033[0m");
 
     long long unsigned int i;
-	
-    for (i = 0; i < strcspn(identifier, " | "); i++){
+
+    for (i = 0; i < strcspn(identifier, " | "); i++) {
         printf("\033[32m%c\033[0m", identifier[i]);
-	}
-	
-	printf("\033[1m\n   Original text:\n\033[0m");
+    }
+
+    printf("\033[1m\n   Original text:\n\033[0m");
 
     for (i = 0; i < current_word.start; i++) {
-            printf("\033[90m%c\033[0m", text[i]);
-        }
+        printf("\033[90m%c\033[0m", text[i]);
+    }
 
     for (i = current_word.start; i < current_word.position; i++) {
-            printf("\033[32m%c\033[0m", text[i]);
-        }
+        printf("\033[32m%c\033[0m", text[i]);
+    }
 
     for (i = current_word.position; i < current_word.end; i++) {
-            printf("\033[104m%c\033[0m", text[i]);
-        }
+        printf("\033[104m%c\033[0m", text[i]);
+    }
 
     for (i = current_word.end; i < strlen(text); i++) {
-            printf("%c", text[i]);
-        }
+        printf("%c", text[i]);
+    }
 
     printf("\033[1m\n\n   Input:\n\033[0m");
 
     for (i = current_word.start; i < current_word.position; i++) {
-            printf("\033[32m%c\033[0m", text[i]);
-        }
+        printf("\033[32m%c\033[0m", text[i]);
+    }
 }
 
 void start_round(
         char identifier[MAX_LENGTH_OF_IDENTIFIER],
         char text[MAX_LENGTH_OF_TEXT])
 {
-	c_w current_word;
+    c_w current_word;
     long long unsigned int i;
     char input[MAX_LENGTH_OF_TEXT], buffer[MAX_LENGTH_OF_TEXT];
     int counter;
@@ -67,7 +70,8 @@ void start_round(
         if (!strncmp(input, "/exit", 5))
             break;
 
-        for (i = current_word.position, counter = 0; i < current_word.end; i++) {
+        for (i = current_word.position, counter = 0; i < current_word.end;
+             i++) {
             if (input[counter++] == text[i]) {
                 current_word.position = i + 1;
             } else {
