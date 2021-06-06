8195b06d367dead59e4833dec50f95650133d7f2
surrepoff <alexander.bessonov548@gmail.com>
2021-06-06 22:52:16 +0700

    Added compare_input_to_text tests

---
+++ test/round_test.c
@@ -0,0 +1,75 @@
+#include <string.h>
+
+#include <lib-keyboard-ninja/auxiliary.h>
+#include <lib-keyboard-ninja/round.h>
+
+#include <ctest.h>
+
+CTEST(compare_input_to_text, test1)
+{
+	c_w current_word;
+    char text[MAX_LENGTH_OF_TEXT];
+    char input[MAX_LENGTH_OF_TEXT];
+	char buffer[MAX_LENGTH_OF_TEXT];
+
+    strcpy(text, "Hello world!");
+	strcpy(input, "Hello");
+	strcpy(buffer, text);
+	
+	current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
+    buffer[strcspn(buffer, " ")] = '@';
+
+    compare_input_to_text(text,input,current_word);
+
+    const int exp = 5;
+
+    ASSERT_EQUAL(exp, (int)current_word.position);
+}
+
+CTEST(compare_input_to_text, test2)
+{
+	c_w current_word;
+    char text[MAX_LENGTH_OF_TEXT];
+    char input[MAX_LENGTH_OF_TEXT];
+	char buffer[MAX_LENGTH_OF_TEXT];
+
+    strcpy(text, "Hello world!");
+	strcpy(input, "Hel");
+	strcpy(buffer, text);
+	
+	current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
+    buffer[strcspn(buffer, " ")] = '@';
+
+    compare_input_to_text(text,input,current_word);
+
+    const int exp = 3;
+
+    ASSERT_EQUAL(exp, (int)current_word.position);
+}
+
+CTEST(compare_input_to_text, test3)
+{
+	c_w current_word;
+    char text[MAX_LENGTH_OF_TEXT];
+    char input[MAX_LENGTH_OF_TEXT];
+	char buffer[MAX_LENGTH_OF_TEXT];
+
+    strcpy(text, "Hello world!");
+	strcpy(input, "Hello ");
+	strcpy(buffer, text);
+	
+	current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
+    buffer[strcspn(buffer, " ")] = '@';
+
+    compare_input_to_text(text,input,current_word);
+
+    const int exp = 5;
+
+    ASSERT_EQUAL(exp, (int)current_word.position);
+}
