1dc12f8efe94795877000310ce8cd293caf1de03
surrepoff <alexander.bessonov548@gmail.com>
2021-06-06 17:22:40 +0700

    Added struct current_word

--- src/lib-keyboard-ninja/round.c
+++ src/lib-keyboard-ninja/round.c
@@ -5,6 +5,12 @@
 #include <lib-keyboard-ninja/auxiliary.h>
 #include <lib-keyboard-ninja/round.h>
 
+struct current_word{
+	long long unsigned int start;
+	long long unsigned int end;
+	long long unsigned int position;
+};
+
 void print_round_interface(char identifier[MAX_LENGTH_OF_IDENTIFIER])
 {
     printf("\033[1mROUND | IDENTIFIER \033[0m");
@@ -18,45 +24,45 @@ void start_round(
         char identifier[MAX_LENGTH_OF_IDENTIFIER],
         char text[MAX_LENGTH_OF_TEXT])
 {
-    long long unsigned int i, start_of_current_word, end_of_current_word,
-            position;
+	struct current_word current_word;
+    long long unsigned int i;
     char input[MAX_LENGTH_OF_TEXT], buffer[MAX_LENGTH_OF_TEXT];
     int counter;
 
     strcpy(buffer, text);
 
-    position = 0;
-    start_of_current_word = 0;
-    end_of_current_word = strcspn(buffer, " ");
+    current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
 
     buffer[strcspn(buffer, " ")] = 64;
 
-    while (position < strlen(text)) {
+    while (current_word.position < strlen(text)) {
         system("clear");
 
         print_round_interface(identifier);
 
         printf("\033[1m\n   Original text:\n\033[0m");
 
-        for (i = 0; i < start_of_current_word; i++) {
+        for (i = 0; i < current_word.start; i++) {
             printf("\033[90m%c\033[0m", text[i]);
         }
 
-        for (i = start_of_current_word; i < position; i++) {
+        for (i = current_word.start; i < current_word.position; i++) {
             printf("\033[32m%c\033[0m", text[i]);
         }
 
-        for (i = position; i < end_of_current_word; i++) {
+        for (i = current_word.position; i < current_word.end; i++) {
             printf("\033[104m%c\033[0m", text[i]);
         }
 
-        for (i = end_of_current_word; i < strlen(text); i++) {
+        for (i = current_word.end; i < strlen(text); i++) {
             printf("%c", text[i]);
         }
 
         printf("\033[1m\n\n   Input:\n\033[0m");
 
-        for (i = start_of_current_word; i < position; i++) {
+        for (i = current_word.start; i < current_word.position; i++) {
             printf("\033[32m%c\033[0m", text[i]);
         }
 
@@ -65,18 +71,18 @@ void start_round(
         if (!strncmp(input, "/exit", 5))
             break;
 
-        for (i = position, counter = 0; i < end_of_current_word; i++) {
+        for (i = current_word.position, counter = 0; i < current_word.end; i++) {
             if (input[counter++] == text[i]) {
-                position = i + 1;
+                current_word.position = i + 1;
             } else {
                 break;
             }
         }
 
-        if (position == end_of_current_word) {
-            position = end_of_current_word + 1;
-            start_of_current_word = end_of_current_word + 1;
-            end_of_current_word = strcspn(buffer, " ");
+        if (current_word.position == current_word.end) {
+            current_word.position = current_word.end + 1;
+            current_word.start = current_word.end + 1;
+            current_word.end = strcspn(buffer, " ");
 
             buffer[strcspn(buffer, " ")] = 64;
         }
