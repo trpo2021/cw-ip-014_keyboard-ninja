b96d6f69907c3f913a975354599831124a9a8445
surrepoff <alexander.bessonov548@gmail.com>
2021-06-06 23:01:17 +0700

    Added shift_current_word tests

--- test/round_test.c
+++ test/round_test.c
@@ -73,3 +73,45 @@ CTEST(compare_input_to_text, test3)
 
     ASSERT_EQUAL(exp, (int)current_word.position);
 }
+
+CTEST(shift_current_word, test1)
+{
+    c_w current_word;
+    char text[MAX_LENGTH_OF_TEXT];
+    char buffer[MAX_LENGTH_OF_TEXT];
+
+    strcpy(text, "Hello world!");
+    strcpy(buffer, text);
+
+    current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
+    buffer[strcspn(buffer, " ")] = '@';
+
+    shift_current_word(buffer, &current_word);
+
+    const int exp = 12;
+
+    ASSERT_EQUAL(exp, (int)current_word.end);
+}
+
+CTEST(shift_current_word, test2)
+{
+    c_w current_word;
+    char text[MAX_LENGTH_OF_TEXT];
+    char buffer[MAX_LENGTH_OF_TEXT];
+
+    strcpy(text, "Hello my friend!");
+    strcpy(buffer, text);
+
+    current_word.position = 0;
+    current_word.start = 0;
+    current_word.end = strcspn(buffer, " ");
+    buffer[strcspn(buffer, " ")] = '@';
+
+    shift_current_word(buffer, &current_word);
+
+    const int exp = 9;
+
+    ASSERT_EQUAL(exp, (int)current_word.end);
+}
