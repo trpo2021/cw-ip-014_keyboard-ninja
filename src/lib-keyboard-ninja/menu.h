da57ca4377a8289d1c7e647ae87c95195015462e
surrepoff <alexander.bessonov548@gmail.com>
2021-06-08 04:04:32 +0700

    Included auxiliary.h

--- src/lib-keyboard-ninja/menu.h
+++ src/lib-keyboard-ninja/menu.h
@@ -1,6 +1,8 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <lib-keyboard-ninja/auxiliary.h>
+
 void result_game(
         char text[MAX_LENGTH_OF_TEXT],
         char identifier[MAX_LENGTH_OF_IDENTIFIER],
