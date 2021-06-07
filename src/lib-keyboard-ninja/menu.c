763d0dc1f34b59394a4e79ea33ce9ad1026d9b69
m1hesha <dan4ikmiheshka@gmail.com>
2021-06-07 21:51:25 +0700

    added a library

--- src/lib-keyboard-ninja/menu.c
+++ src/lib-keyboard-ninja/menu.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <lib-keyboard-ninja/menu.h>
 #include <lib-keyboard-ninja/auxiliary.h>
 #include <lib-keyboard-ninja/round.h>
 #include <lib-keyboard-ninja/text.h>
