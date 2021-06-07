efb17007add4ec81b9b7dc5518666e94df341e68
surrepoff <alexander.bessonov548@gmail.com>
2021-06-08 04:03:56 +0700

    Formated code

--- src/lib-keyboard-ninja/menu.h
+++ src/lib-keyboard-ninja/menu.h
@@ -1,7 +1,11 @@
 #ifndef MENU_H
 #define MENU_H
 
-void result_game(char text[MAX_LENGTH_OF_TEXT], char identifier[MAX_LENGTH_OF_IDENTIFIER], int error_counter, double time );
+void result_game(
+        char text[MAX_LENGTH_OF_TEXT],
+        char identifier[MAX_LENGTH_OF_IDENTIFIER],
+        int error_counter,
+        double time);
 void menu_template();
 void start_game();
 void help_game();
