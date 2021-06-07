c90334fe320021950eb8a9b9e202341a4687432f
m1hesha <dan4ikmiheshka@gmail.com>
2021-06-08 03:49:52 +0700

    updated menu.h-added result_game

--- src/lib-keyboard-ninja/menu.h
+++ src/lib-keyboard-ninja/menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+void result_game(char text[MAX_LENGTH_OF_TEXT], char identifier[MAX_LENGTH_OF_IDENTIFIER], int error_counter, double time );
 void menu_template();
 void start_game();
 void help_game();
